65a0ae51222bf762ea3388c55bf77645ced15558
Razieh Abedi <raziyeabedi@gmail.com>
2023-03-12 16:38:08 +0800

    added useEffcet and useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -27,11 +27,7 @@ function App() {
   //    .catch(console.log("error");)
   // };
 
-  // useEffect(() => {
-  //   fetchMoviesHandler();
-  // }, []);
-
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -55,7 +51,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
